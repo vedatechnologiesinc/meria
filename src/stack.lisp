3f313659e16e7d1345b9024db8b42d9584557065
Rommel Martínez <ebzzry@icloud.com>
2026-01-09 00:06:18 +0800

    [stack] use new macro system

--- src/stack.lisp
+++ src/stack.lisp
@@ -15,36 +15,34 @@
 
 This should be a real, T (always extend), or NIL (never extend).")
 
+;; TODO
 (defm with-stack-extensions ((&key
                                 (limit '*stack-limit* limitp)
                                 (use-stack-limit nil))
                              &body forms)
   "Control stack extensions in LW.
 
-If LIMIT is given this is the limit (see *STACK-LIMIT* for what it can
-be).  If USE-STACK-LIMIT is given as true, then the dynamic value of
-*STACK-LIMIT* will be used instead: this overrides any LIMIT value."
+If LIMIT is given this is the limit (see *STACK-LIMIT* for what it can be). If USE-STACK-LIMIT is given as true, then the dynamic value of *STACK-LIMIT* will be used instead: this overrides any LIMIT value."
   ;; It is tempting to allow the limit to be a predicate, but I think
   ;; calling functions at this point is probably not a good idea.
   (when (and use-stack-limit limitp)
     (warn "both LIMIT and USE-STACK-LIMIT given: will use *STACK-LIMIT*"))
-  (with-gensyms (lim)
-    (if use-stack-limit
-        `(handler-bind ((stack-overflow
+  (if use-stack-limit
+      `(handler-bind ((stack-overflow
+                        (lambda (c)
+                          (when (and *stack-limit*
+                                     (or (eq *stack-limit* t)
+                                         (< (current-stack-length) *stack-limit*)))
+                            (let ((r (find-restart 'continue c)))
+                              (when r (invoke-restart r)))))))
+         ,@forms)
+      `(let ((,g!lim ,limit))
+         (check-type ,g!lim (or boolean real))
+         (handler-bind ((stack-overflow
                           (lambda (c)
-                            (when (and *stack-limit*
-                                       (or (eq *stack-limit* t)
-                                           (< (current-stack-length) *stack-limit*)))
+                            (when (and ,g!lim
+                                       (or (eq ,g!lim t)
+                                           (< (current-stack-length) ,g!lim)))
                               (let ((r (find-restart 'continue c)))
                                 (when r (invoke-restart r)))))))
-           ,@forms)
-        `(let ((lim ,limit))
-           (check-type lim (or boolean real))
-           (handler-bind ((stack-overflow
-                            (lambda (c)
-                              (when (and lim
-                                         (or (eq lim t)
-                                             (< (current-stack-length) lim)))
-                                (let ((r (find-restart 'continue c)))
-                                  (when r (invoke-restart r)))))))
-             ,@forms)))))
+           ,@forms))))
